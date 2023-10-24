7fb8fb14f555d9e2e3287ae86fb5a520a53bf4c3
miriam <miriamsilvaaa484@gmail.com>
2023-10-23 22:25:04 -0700

    added resume link

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,16 +1,41 @@
 import React from 'react';
 
-const Resume = () => {
-  const resumeUrl = '/resumes/techresume.pdf';
+class DownloadButton extends React.Component {
+  // function to handle the file download
+  downloadFile = () => {
+    // create an anchor element and set the file URL and file name
+    const link = document.createElement('a');
+    link.href = '/resumes/techresume.html' // replace with your file's URL or path
+    link.setAttribute('download', 'techresume.html');
+    link.style.display = 'none';
+ 
+    // add the anchor element to your document
+    document.body.appendChild(link);
 
+    // trigger the download by stimulating click
+    link.click();
+
+    // clean up: remove the anchor element from the DOM 
+    document.body.removeChild(link);
+  };
+
+  render() {
+    return (
+      <button onClick={this.downloadFile}>
+        Download
+      </button>
+    );
+  }
+}
+
+// Main app component
+function App() {
   return (
-    <div>
-      <h1>Resume</h1>
-      <a href={resumeUrl} download="techresume.pdf">
-        Download Resume
-      </a>
+    <div className="App">
+      {/* render the DownloadButton component */}
+      <DownloadButton />
     </div>
   );
-};
+}
 
-export default Resume;
+export default App;
