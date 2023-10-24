b3d12df68f8e15719ce8971934ea4ce8ebd4dbd1
miriam <miriamsilvaaa484@gmail.com>
2023-10-23 23:10:30 -0700

    fixed comments

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -3,19 +3,15 @@ import React from 'react';
 class DownloadButton extends React.Component {
   // function to handle the file download
   downloadFile = () => {
-    // create an anchor element and set the file URL and file name
     const link = document.createElement('a');
-    link.href = '/resumes/techresume.html' // replace with your file's URL or path
-    link.setAttribute('download', 'techresume.html');
+    link.href = '/resumes/techresume.pdf'
+    link.setAttribute('download', 'techresume.pdf');
     link.style.display = 'none';
  
-    // add the anchor element to your document
     document.body.appendChild(link);
 
-    // trigger the download by stimulating click
     link.click();
 
-    // clean up: remove the anchor element from the DOM 
     document.body.removeChild(link);
   };
 
